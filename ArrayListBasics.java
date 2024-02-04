3bf5f187a54b2b7b544df7eeb86a7c2b7dec57be
Jashin Bhatia <jashinbhatia4567@gmail.com>
2024-02-05 00:32:19 +0530

    Add files via upload

---
+++ ArrayListBasics.java
@@ -0,0 +1,36 @@
+package LearnTheBasics;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListBasics {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        // input for Arraylist
+
+        for(int index = 0; index < 5; index++) {
+
+            int num = input.nextInt();
+            list.add(num);
+
+        }
+
+        // output for Arraylist
+
+        System.out.println(list);
+
+        for(int index = 0; index < 5; index++) {
+
+            System.out.println(list.get(index));
+
+        }
+
+        System.out.println("Element at 3 is : " + list.get(3));
+
+    }
+
+}
